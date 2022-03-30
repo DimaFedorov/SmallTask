9346f3be8bc36b90867bcd3f4337d1b3de5661d6
Dmytro <dim.fed.13@gmail.com>
2022-03-30 15:23:24 +0300

    Finished small task

--- SmallTask/Controllers/HomeController.cs
+++ SmallTask/Controllers/HomeController.cs
@@ -1,32 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SmallTask.Models;
-using System.Diagnostics;
 
 namespace SmallTask.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public IActionResult Index(string name)
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
+            if(name != null)
+            {
+                ViewData["Message"] = "Hi, " + name;
+                return View("HiName");
+            }
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
